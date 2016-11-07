f39cbdbc142c06f9077af1e24c3aa4b20f776548
Brandon John-Freso <brandonjf@gmail.com>
2016-11-07 09:40:13 -0500

    cleanup

--- app/src/main/java/com/vimeo/cleancode/views/BrowseActivity.java
+++ app/src/main/java/com/vimeo/cleancode/views/BrowseActivity.java
@@ -40,47 +40,4 @@ public class BrowseActivity extends AppCompatActivity {
         mBrowseViewModel.getData();
         mBinding.browseRvVideos.setAdapter(mBrowseViewModel.getVideoAdapter());
     }
-    private class StaffPicksAsyncTask extends AsyncTask<Void, Void, ArrayList<JSONObject>> {
-
-        @Override
-        protected ArrayList<JSONObject> doInBackground(Void... params) {
-            String url = "https://api.vimeo.com/channels/staffpicks/videos";
-            String token = "bearer b8e31bd89ba1ee093dc6ab0f863db1bd";
-            ArrayList<JSONObject> videos = new ArrayList<>();
-
-//            StringBuilder builder = new StringBuilder();
-//            HttpClient client = new DefaultHttpClient();
-//            HttpGet httpGet = new HttpGet(url);
-//            httpGet.addHeader("Authorization", token);
-//            try {
-//                HttpResponse response = client.execute(httpGet);
-//                HttpEntity entity = response.getEntity();
-//                InputStream content = entity.getContent();
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    builder.append(line);
-//                }
-//                JSONObject object = new JSONObject(builder.toString());
-//                JSONArray data = object.getJSONArray("data");
-//                for (int i = 0; i < data.length(); i++) {
-//                    videos.add(data.getJSONObject(i));
-//                }
-//            } catch (ClientProtocolException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-            return videos;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<JSONObject> result) {
-            items = result;
-            mAdapter.addAll(items);
-            mAdapter.notifyDataSetChanged();
-        }
-    }
 }
